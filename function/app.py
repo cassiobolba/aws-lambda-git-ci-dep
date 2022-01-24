cb39ec3438f4d574d465b46f4b19be49dba244c9
Cassio Bolba <cassio.bolba@gmail.com>
2022-01-25 00:05:46 +0100

    update function

--- function/app.py
+++ function/app.py
@@ -1,10 +1,11 @@
-import json
+import requests
 
-def lambda_handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello worlddd",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+def lambda_handler(event, context, city="Porto Alegre"):
+
+    api_key = "868a26a88dcad371f4205a319f26be8c"
+
+    url = "http://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid="+ api_key
+
+    json_data = requests.get(url).json()
+
+    return f"Current Temperature for {city} is {json_data['main']['temp']/10}"
