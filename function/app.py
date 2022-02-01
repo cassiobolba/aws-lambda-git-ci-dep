8b6ab42b67049beeed07ae1fd389f4a27fc4b166
Cassio De Bolba <44919506+cassiobolba@users.noreply.github.com>
2022-02-01 21:35:45 +0100

    Update app.py

--- function/app.py
+++ function/app.py
@@ -9,7 +9,6 @@ def lambda_handler(event, context):
     
     api_key = "868a26a88dcad371f4205a319f26be8c-io"
 
-<<<<<<< HEAD
     url = "http://api.openweathermap.org/data/2.5/weather?q="+ str(city) +"&appid="+ api_key
 
     json_data = requests.get(url).json()
@@ -22,12 +21,3 @@ def lambda_handler(event, context):
             "headers": None,
             "body": event
             }
-=======
-    api_key = ""
-
-    url = "http://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid="+ api_key
-
-    json_data = requests.get(url).json()
-
-    return f"Current Temperature for {city} is {json_data['main']['temp']/10}"
->>>>>>> 03fc4a8fd5da9db2b77e633fe225f2ae21f975a7
